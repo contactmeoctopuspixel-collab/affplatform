80dcf050b280b5119099260e68ee48474cea9b12
AffPlatform Admin <admin@affplatform.com>
2026-05-19 14:32:13 +0100

    Add expenses collection to database

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -19,6 +19,7 @@ const db = {
   events:      Datastore.create(opts("events.db")),
   daily_stats: Datastore.create(opts("daily_stats.db")),
   conversions: Datastore.create(opts("conversions.db")),
+  expenses:    Datastore.create(opts("expenses.db")),
   chat:        Datastore.create(opts("chat.db")),
 };
 
